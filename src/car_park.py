046d87479185bcda910a634ebc2a62b602f30871
20135031 <20135031@tafe.wa.edu.au>
2025-06-11 19:23:20 +0800

    feature: added constructor to CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,15 @@
 class CarPark:
-    pass
+    def __init__(self,
+                 location = "Unknown",
+                 capacity = 100,
+                 plates = None,
+                 displays = None,
+                 sensors = None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.displays = displays or []
+        self.sensors = sensors or []
+
+    def __str__(self):
+        return f"Car park at {self.location}, with {self.capacity} bays."
