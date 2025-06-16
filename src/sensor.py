da02312fbcb80262977ee25ebbd19531b51ad82f
20135031 <20135031@tafe.wa.edu.au>
2025-06-16 18:03:24 +0800

    feature: created constructors for display and sensor

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,12 @@
 class Sensor:
-    pass
+    def __init__(self, id, car_park, is_active=False):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"Sensor {self.id} is {self.is_active}"
+
+class EntrySensor(Sensor):
+
+class ExitSensor(Sensor):
